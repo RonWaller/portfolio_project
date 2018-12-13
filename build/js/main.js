497400bb0bfb2e92557631ab7c41963e2f0b51a1
RonWaller <ron.waller7@gmail.com>
2018-12-12 19:50:46 -0500

    Finished media queries

--- build/js/main.js
+++ build/js/main.js
@@ -1,10 +1,11 @@
 const menubars = document.querySelector('#menubars');
-const menu = document.querySelector('#menu');
+const menu = document.querySelector('#slidemenu');
 const close = document.querySelector('#close');
 const navItems = document.querySelectorAll('.menuitems li');
 
 menubars.addEventListener('click', toggleMenu);
 close.addEventListener('click', removeClass);
+
 
 function toggleMenu() {
   menu.classList.add('show');
